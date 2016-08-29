e83c23386303c98ca0da64fb44f8819f8400fc2c
eliotwinder <musicismymistress@gmail.com>
2016-08-29 09:16:10 -0700

    backbone of frontend

--- app.py
+++ app.py
@@ -1,23 +1,54 @@
 # !/usr/bin/env python
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+from models import get_models
+
+app = Flask(
+    __name__,
+    static_url_path=''
+)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
+Player, Game = get_models(db)
 
 
-@app.route('/player_/<player_name>', methods=['GET', 'POST'])
-def PlayerHandler(player_name):
+@app.route('/')
+def HomepageHandler():
+    return app.send_static_file('index.html')
+
+
+@app.route('/players', methods=['GET'])
+def AllPlayersRoute():
     if request.method == 'GET':
-        return get_player__info(player_name)
-    if request.method == 'POST':
-        return create_player_(player_name)
+        print jsonify(Player.get_all_players()).data
+        return jsonify(Player.get_all_players())
 
 
-@app.route('/game', methods=['POST'])
-def GameHandler(player_name):
-    if request.method == 'GET':
-        return get_game_info(game_id)
-    if request.method == 'POST':
-        return create_game(**request.body)
+@app.route('/player', methods=['POST'])
+def CreatePlayerHandler():
+    try:
+        playername = request.form['playername']
+        player = Player(playername)
+        db.session.add(player)
+        db.session.commit()
+        return jsonify(player.get_stats())
+    except:
+        # most likely, the user is already created
+        return 403
+
+
+@app.route('/player/<playername>', methods=['GET'])
+def GetPlayerInfoHandler(player_name):
+    return Player.get_players_info(player_name)
+
+# @app.route('/game', methods=['GET', 'POST'])
+# def GameHandler(player_name):
+#     if request.method == 'GET':
+#         # return get_game_info(game_id)
+#     if request.method == 'POST':
+#         # return create_game(**request.body)
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -5,99 +5,107 @@ to initialize database do:
 """
 from collections import defaultdict
 
-from app import db
 
+def get_models(db):
+    class GamePlayer(db.Model):
+        __tablename__ = 'game_player'
+        id = db.Column(db.Integer, primary_key=True)
+        game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
+        player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
+        win = db.Column(db.Boolean, nullable=False)
 
-class GamePlayer(db.Model):
-    __tablename__ = 'game_player'
-    id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
-    player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
-    win = db.Column(db.Boolean, nullable=False)
+        player = db.relationship('Player', back_populates='games')
+        game = db.relationship('Game', back_populates='players')
 
-    player = db.relationship('Player', back_populates='games')
-    game = db.relationship('Game', back_populates='players')
+        @property
+        def playername(self):
+            return self.player.playername
 
-    @property
-    def playername(self):
-        return self.player.playername
+        @property
+        def gametype(self):
+            return self.game.gametype
 
-    @property
-    def gametype(self):
-        return self.game.gametype
+    class Player(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        playername = db.Column(db.String(80), unique=True)
+        games = db.relationship('GamePlayer', back_populates='player')
 
+        def __repr__(self):
+            return '<Player %s>' % self.playername
 
-class Player(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    playername = db.Column(db.String(80), unique=True)
-    games = db.relationship('GamePlayer', back_populates='player')
+        def __init__(self, playername):
+            self.playername = playername
+            db.session.add(self)
 
-    def __repr__(self):
-        return '<Player %s>' % self.playername
+        def to_dict(self):
+            return {
+                'id': self.id,
+                'playername': self.playername,
+            }
 
-    def __init__(self, playername):
-        self.playername = playername
-        db.session.add(self)
+        @classmethod
+        def get_all_players(self):
+            players = Player.query.all()
+            return [player.get_stats() for player in players]
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'playername': self.playername,
-        }
+        def get_stats(self):
+            '''
+            returns a dict with key game type and value:
+            {'wins': x, 'loses': y}
+            also has one key, 'player_info' with aself-explanatory value
+            '''
+            stats = defaultdict(dict)
+            for game in self.games:
+                type_stats = stats[game.gametype]
+                if game.win:
+                    type_stats['wins'] = type_stats.get('wins', 0) + 1
+                else:
+                    type_stats['losses'] = type_stats.get('losses', 0) + 1
+            total_wins = 0
+            total_losses = 0
+            for game_type, type_dict in stats.iteritems():
+                total_wins += type_dict.get('wins', 0)
+                total_losses += type_dict.get('losses', 0)
+            stats['total'] = {'wins': total_wins, 'losses': total_losses}
+            return {
+                "playername": self.playername,
+                "stats": stats
+            }
 
-    def get_stats(self):
-        '''
-        returns a dict with key game type and value: {'wins': x, 'loses': y}
-        '''
-        stats = defaultdict(dict)
-        for game in self.games:
-            type_stats = stats[game.gametype]
-            if game.win:
-                type_stats['wins'] = type_stats.get('wins', 0) + 1
-            else:
-                type_stats['losses'] = type_stats.get('losses', 0) + 1
-        total_wins = 0
-        total_losses = 0
-        for game_type, type_dict in stats.iteritems():
-            total_wins += type_dict.get('wins', 0)
-            total_losses += type_dict.get('losses', 0)
-        stats['total'] = {'wins': total_wins, 'losses': total_losses}
-        return stats
+    class Game(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        gametype = db.Column(db.String(80))
+        players = db.relationship(
+            'GamePlayer',
+            back_populates='game',
+        )
 
+        def __init__(self, gametype, winners, losers):
+            self.gametype = gametype
+            for playername in winners:
+                self.add_player(playername, win=True)
+            for playername in losers:
+                self.add_player(playername, win=False)
+            db.session.add(self)
 
-class Game(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    gametype = db.Column(db.String(80))
-    players = db.relationship(
-        'GamePlayer',
-        back_populates='game',
-    )
+        def __repr__(self):
+            return ('< Game %s: %s beat %s>' %
+                    (self.gametype, self.winners, self.losers))
 
-    def __init__(self, gametype, winners, losers):
-        self.gametype = gametype
-        for playername in winners:
-            self.add_player(playername, win=True)
-        for playername in losers:
-            self.add_player(playername, win=False)
-        db.session.add(self)
+        @property
+        def winners(self):
+            return [player.playername
+                    for player in self.players if player.win]
 
-    def __repr__(self):
-        playernames = [p.player.playername for p in self.players]
-        return ('< Game %s: %s beat %s>' %
-                (self.gametype, self.winners, self.losers))
+        @property
+        def losers(self):
+            return [player.playername
+                    for player in self.players if not player.win]
 
-    @property
-    def winners(self):
-        return [player.playername
-                for player in self.players if player.win]
+        def add_player(self, playername, win):
+            game_player = GamePlayer(win=win)
+            player = Player.query.filter_by(playername=playername).first()
+            game_player.player = player
+            self.players.append(game_player)
 
-    @property
-    def losers(self):
-        return [player.playername
-                for player in self.players if not player.win]
-
-    def add_player(self, playername, win):
-        game_player = GamePlayer(win=win)
-        player = Player.query.filter_by(playername=playername).first()
-        game_player.player = player
-        self.players.append(game_player)
+    return Player, Game
